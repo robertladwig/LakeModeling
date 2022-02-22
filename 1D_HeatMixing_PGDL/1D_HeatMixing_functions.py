1816e756a0286a138d5b2f461e0e7ff1b70e4d9e
cbuelo <cbuelo10@gmail.com>
2022-02-22 16:23:23 -0600

    start python functions

---
+++ 1D_HeatMixing_PGDL/1D_HeatMixing_functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+## function to calculate density from temperature
+def calc_dens(wtemp):
+    dens = (999.842594 + (6.793952 * 1e-2 * wtemp) - (9.095290 * 1e-3 *wtemp**2) + 
+      (1.001685 * 1e-4 * wtemp**3) - (1.120083 * 1e-6* wtemp**4) + 
+      (6.536336 * 1e-9 * wtemp**5))
+    return dens
+
+## this is our attempt for turbulence closure, estimating eddy diffusivity
+def eddy_diffusivity(rho, depth, g, rho_0, ice, area):
+    buoy = np.ones(nx) * 7e-5
+    buoy[:-1] = np.abs(rho[1:] - rho[:-1]) / (depth[1:] - depth[:-1]) * g / rho_0
+    buoy[-1] = buoy[-2]
+        
+    low_values_flags = buoy < 7e-5  # Where values are low
+    buoy[low_values_flags] = 7e-5
+    
+    if ice == True:
+      ak = 0.000898
+    else:
+      ak = 0.00706 *( max(area)/1E6)**(0.56)
+    
+    kz = ak * (buoy)**(-0.43)
+    return(kz)
