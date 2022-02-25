6f5c2cc2a8968723753fd4a524bad6de44f9e73b
cbuelo <cbuelo10@gmail.com>
2022-02-24 19:04:54 -0600

    added get_hypsography function

--- 1D_HeatMixing_PGDL/1D_HeatMixing_functions.py
+++ 1D_HeatMixing_PGDL/1D_HeatMixing_functions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from math import pi, exp
+from scipy.interpolate import interp1d
 
 ## function to calculate density from temperature
 def calc_dens(wtemp):
@@ -62,3 +63,36 @@ def provide_meteorology(meteofile, secchifile, windfactor):
     secview['kd'] = secview.set_index('sampledate')['kd'].interpolate(method="linear").values
     
     return([daily_meteo, secview])
+  
+def initial_profile(initfile, nx, dx, depth, processed_meteo):
+  meteo = processed_meteo
+  startDate = meteo['date'].min()
+  obs = pd.read_csv(initfile)
+  obs['datetime'] = pd.to_datetime(obs['datetime'])
+  obs['ditt'] = abs(obs['datetime'] - startDate)
+  init_df = obs.loc[obs['ditt'] == obs['ditt'].min()]
+  if max(depth) > init_df.Depth_meter.max():
+    lastRow = init_df.loc[init_df.Depth_meter == init_df.Depth_meter.max()]
+    init_df = pd.concat([init_df, lastRow], ignore_index=True)
+    init_df.loc[init_df.index[-1], 'Depth_meter'] = max(depth)
+    
+  profile_fun = interp1d(init_df.Depth_meter.values, init_df.Water_Temperature_celsius.values)
+  out_depths = np.linspace(0, nx*dx, nx) # these aren't actually at the 0, 1, 2, ... values, actually increment by 1.0412; make sure okay
+  u = profile_fun(out_depths)
+  
+  # TODO implement warning about profile vs. met start date
+  
+  return(u)
+
+def get_hypsography(hypsofile, dx, nx):
+  hyps = pd.read_csv(hypsofile)
+  out_depths = np.linspace(1, nx*dx, nx)
+  area_fun = interp1d(hyps.Depth_meter.values, hyps.Area_meterSquared.values)
+  area = area_fun(out_depths)
+  area[area == 0] = 1e-2 # TODO: confirm this is correct
+  depth = np.linspace(1, nx*dx, nx)
+  
+  volume = 0.5 * (area[:-1] + area[1:]) * np.diff(depth)
+  volume = np.append(volume, 1000)
+  
+  return([area, depth, volume])
