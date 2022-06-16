63c476ed6a034a3d8508cc8c2a4c4788f9a43d96
robertladwig <ladwigjena@gmail.com>
2022-06-16 16:20:06 -0500

    working

--- 1D_HeatMixing_PGDL/MCL/run_mcl.R
+++ 1D_HeatMixing_PGDL/MCL/run_mcl.R
@@ -11,31 +11,25 @@ set.seed(123)
 
 input_df <- read_csv('../output/meteorology_input.csv')
 output_df <- read_csv('../output/temp_total04.csv')
+buoy <- read_csv('../output/buoyancy.csv')
+observed <- read_csv('../output/observed_temp.csv')
 
-input_df <- input_df %>%
+buoy <- buoy %>%
   select(-time)
+input_df <- cbind(input_df,buoy) %>%
+  select(-c(time,Area_m2,'n2S-2_25'))
 output_df <- output_df %>%
+  select(-time)
+observed <- observed %>%
   select(-time)
 
 input <- as.matrix(cbind(input_df[2:nrow(input_df),], output_df[1:(nrow(output_df)-1),]))
 target <- as.matrix(output_df[2:nrow(output_df),])
+target <- as.matrix(obsrved[2:nrow(observed),])
 
 # network <- keras_model_sequential() %>%
 #   layer_dense(units = 15, activation = "sigmoid", input_shape = c(34)) %>%
 #   layer_dense(units = 25)
-
-
-model = keras_model_sequential() %>% 
-  layer_dense(units=200, activation="relu", input_shape=c(34)) %>% 
-  layer_dropout(rate = 0.2) %>% 
-  layer_dense(units=32, activation = "relu") %>%
-  layer_dropout(rate = 0.1) %>% 
-  layer_dense(units=1, activation="linear")
-
-summary(network)
-
-network %>% compile(optimizer = 'adam', 
-                    loss = "mse", metrics = c("mean_absolute_error"))
 
 idx <- floor(nrow(input) * 0.6)
 train_features <- input[1:idx,]
@@ -44,9 +38,33 @@ train_target <- target[1:idx,]
 test_features <- input[(idx+1):nrow(input),]
 test_target <- target[(idx+1):nrow(input),]
 
-network %>% fit(train_features, train_target, epochs = 100, validation_split = 0.4)
+normalize <- function(x){
+  num <- x - min(x)
+  denom <- max(x) - min(x)
+  return(num/denom)
+}
 
-history <- network %>% fit(train_features, train_target, epochs = 100)
+# normalise stuff?
+# train_features <- as.matrix(apply(train_features, 2, normalize))
+# train_target <- as.matrix(apply(train_target, 2, normalize))
+# test_features <- as.matrix(apply(test_features, 2, normalize))
+# test_target <- as.matrix(apply(test_target, 2, normalize))
+
+network = keras_model_sequential() %>% 
+  layer_dense(units=32, activation="gelu", input_shape=c(57)) %>% 
+  # layer_dropout(rate = 0.2) %>% 
+  layer_dense(units=32, activation = "gelu") %>%
+  # layer_dropout(rate = 0.1) %>% 
+  layer_dense(units=25, activation="linear")
+
+summary(network)
+
+network %>% compile(optimizer = 'adam', 
+                    loss = "mse", metrics = c("mean_absolute_error"))
+
+network %>% fit(train_features, train_target, epochs = 1000, validation_split = 0.4)
+
+history <- network %>% fit(train_features, train_target, epochs = 1000)
 plot(history)
 
 scores = network %>% evaluate(train_features, train_target, verbose = 0)
@@ -56,5 +74,5 @@ pred_train <- network %>% predict(train_features, batch_size = 128)
 
 pred_test <- network %>% predict(test_features, batch_size = 128)
 
-# Confusion matrix
-table(test_target, classes)
+val.scores = network %>% evaluate(test_features, test_target, verbose = 0)
+print(val.scores)
